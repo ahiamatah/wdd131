f7ef90250d76477ca6eb4ceb217fef84847cc1e2
ahiamatah <ahiamatah@gmail.com>
2025-05-10 08:27:05 +0000

    ...

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -18,8 +18,5 @@ let hours = date.getHours();
 let minutes = date.getMinutes();
 let seconds = date.getSeconds();
 
-if (hours < 10) hours = '0' + hours;
-if (minutes < 10) minutes = '0' + minutes;
-if (seconds < 10) seconds = '0' + seconds;
 
 docFullDateAndTime.innerHTML = (` ${day}/${month+1}/${year} ${hours}:${minutes}:${seconds}`);
