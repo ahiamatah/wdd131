f1d67e97fbaa56c162d658df8e5d83b1fbdd35d8
ahiamatah <ahiamatah@gmail.com>
2025-06-19 07:29:13 +0000

    added video page

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -7,3 +7,13 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
+
+function toHome(){
+    const home = document.querySelector('#logo-image');
+    home.addEventListener('click', () => {
+        window.location.href = 'index.html';
+    })
+}
+
+// Call the function after the DOM is loaded
+document.addEventListener('DOMContentLoaded', toHome);
